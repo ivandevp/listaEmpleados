61bc97ab08cae6efa3303df424f37b8a165f90b0
Zitle-Nancy <ancizjhotmail.com>
2017-09-08 17:51:19 -0500

    obtiene la api y los datos en consola

---
+++ controllers/appController.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('listaDatos', ['testService', 'ui.bootstrap']);
+
+app.controller('testController', ['$scope', 'testRequest', '$uibModal', obtieneDatos]);
+
+function obtieneDatos($scope, testRequest, $uibModal) {
+    $scope.obtenerNombres = function () {
+        testRequest.post().then(function (response) {
+            $scope.posts = response.data;
+            console.log($scope.posts);
+        });
+    }
+
+}
---
+++ services/servicioApi.js
@@ -0,0 +1,11 @@
+﻿angular.module('testService', [])
+    .factory('testRequest', function ($http) {
+        var ruta = "https://jsonplaceholder.typicode.com/users";
+        return {
+            post: function () {
+                global = $http.get(ruta);
+                //console.log(global);
+                return global;
+            }
+        }
+    });
