399c3ffcdb992903fcb6e2773648701e7b971002
nancyzj <ancizj@hotmail.com>
2017-09-09 22:03:32 -0500

    hace un ciclo infinito al obtener el api

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,13 +1,48 @@
-﻿var app = angular.module('listaDatos', ['testService', 'ui.bootstrap']);
+﻿var app = angular.module('listaDatos', ['testService', 'ui.bootstrap','ngRoute']);
 
-app.controller('testController', ['$scope', 'testRequest', '$uibModal', obtieneDatos]);
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: '../index.html',
+			controller: 'testController'
+		})
+		.when('/mostrar-datos',{
+			templateUrl: '../tablaUsuarios.html',
+			controller: 'datosTabla'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+/*podemos hacer la funcion dentro del controller o podemos inyectarla, y junto con los parametros que va a ocupar*/
+app.controller('testController', ['$scope', 'testRequest', '$uibModal','$location', obtieneDatos]);
 
-function obtieneDatos($scope, testRequest, $uibModal) {
-    $scope.obtenerNombres = function () {
+function obtieneDatos($scope, testRequest, $uibModal, $location) {
+	// $scope.id = "";
+	// $scope.nombre = "";
+	// $scope.userName = "";
+	// $scope.email = "";
+    $scope.obtenerNombres = function (path) {
         testRequest.post().then(function (response) {
-            $scope.posts = response.data;
-            console.log($scope.posts);
+        	// post tiene los datos
+            $scope.post = response.data[0];
+            seccionStorage.setTime('datos',
+            		JSON.stringify({
+            			id: $scope.post.id,
+            			nombre: $scope.post.name,
+            			userName: $scope.post.username,
+            			email: $scope.post.email
+            		})
+            	);
+        // mostrar datos en la tabla
+    	$location.path(path);
         });
     }
 
+}
+
+app.controller('datosTabla',['$scope',mostrarDatos]);
+
+function mostrarDatos($scope) {
+	$scope.objetoUsuarios = JSON.parse(seccionStorage.getItem('datos'));
 }
